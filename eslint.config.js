a9b7f948ed018c7e6de6bc75814b35c1a2fc714f
Allan Andrade <allanpleao@gmail.com>
2024-09-13 17:12:19 -0300

    gitignore config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import globals, { browser } from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -17,6 +17,8 @@ export default [
         sourceType: 'module',
       },
     },
+  env: {browser: true},
+
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
