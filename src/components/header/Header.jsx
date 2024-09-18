a71273ae7d9045d015a1022ee5977dbc2c59ca86
Allan Andrade <allanpleao@gmail.com>
2024-09-18 16:01:05 -0300

    styles: updated header elements

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import logo from '../../assets/logo.png'
 
 const Header = () => {
   return (
     <header className={styles.header}>
+            <div>
+              <Link to={'/'}><img className={styles.logo} src={logo}/></Link>
+            <Link to={'/'} className={styles.logoText}>Aperture </Link>
+            </div>
         <nav className={styles.nav}>
+          
             <Link to={'/login'}>Login</Link>
             <Link to={'/login'}>About</Link>
             <Link to={'/login'}>Some text</Link>
