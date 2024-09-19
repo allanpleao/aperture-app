7ba5c29fb666c3894898081ad7d4f6791ff136c4
Allan Andrade <allanpleao@gmail.com>
2024-09-19 16:43:57 -0300

    added onChange event to input component

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './Input.module.css'
 
-const Input = ({ label, value, type }) => {
+const Input = ({ onChange, label, value, type }) => {
   return (
     <div className={styles.input}>
         <label htmlFor="">{label}</label>
-        <input type={type} value={value}/>
+        <input onChange={onChange} type={type} value={value}/>
     </div>
   )
 }
