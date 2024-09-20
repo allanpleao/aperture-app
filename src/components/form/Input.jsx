f0765e1ed15339622b36e1369a2e156b15600ffd
Allan Andrade <allanpleao@gmail.com>
2024-09-20 17:26:00 -0300

    added react hook form to the create account form

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './Input.module.css'
 
-const Input = ({ onChange, label, value, type }) => {
+const Input = React.forwardRef(({onBlur, error, onChange, label, value, type }, ref) => {
   return (
     <div className={styles.input}>
         <label htmlFor="">{label}</label>
-        <input onChange={onChange} type={type} value={value}/>
+        <input onBlur={onBlur} onChange={onChange} type={type} value={value}/>
+        <p style={{ color: 'orange'}}>{error}</p>
     </div>
   )
-}
-
+})
+Input.displayName = 'Input';
 export default Input
--- src/components/login/CreateAccount.jsx
+++ src/components/login/CreateAccount.jsx
@@ -1,41 +1,83 @@
-import React, { useState } from 'react'
-import Input from '../form/Input'
-import Button from '../form/Button'
-import styles from './CreateAccount.module.css'
-import { auth } from '../../../firebaseConfig';
-import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { setUser } from '../../redux/store/slices/authSlice';
-import { useDispatch } from 'react-redux'
-import Error from '../helper/Error'
+import React, { useState } from "react";
+import Input from "../form/Input";
+import Button from "../form/Button";
+import styles from "./CreateAccount.module.css";
+import { auth } from "../../../firebaseConfig";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { setUser } from "../../redux/store/slices/authSlice";
+import { useDispatch } from "react-redux";
+import Error from "../helper/Error";
+import { useForm } from "react-hook-form";
 
 const CreateAccount = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-  const dispatch = useDispatch();
 
-  const handleCreateAccount = async(event) => {
-    event.preventDefault();
-    try {
-      const userCredential = await createUserWithEmailAndPassword(auth, password, email);
-      console.log('Usuário criado', userCredential.user);
-      dispatch(setUser(userCredential.user))
-    } catch (error) {
-      console.error('Erro ao criar usuário', error)
-      setError(error.message)
+
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = useForm({
+    mode: 'onBlur'
+  });
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    if (!email) {
+      return 'email é obrigatório'
+    }
+     if (!emailRegex.test(email)) {
+      return "Digite um email válido";
+    }
+    return true;
+  }
+  const validatePassword = (value) => {
+    if(!password) {
+      return 'senha é obrigatória'
     }
   }
-  
-  return (
-    <form onSubmit={handleCreateAccount} className={styles.createContainer}>
-      <h2 className={styles.createTitle}>Criar conta</h2>
-      <Input value={email} onChange={({ target }) => setEmail(target.value)} label='Email'/>
-      <Input value={password} onChange={({ target }) => setPassword(target.value)} label='Senha' />
-      <Input label='Confirmar senha' />
-      <Button>Criar conta</Button>
-      {error && <Error error={error} />}
-    </form>
-  )
-}
 
-export default CreateAccount
+  const handleCreateAccount = async (event) => {
+    event.preventDefault();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        password,
+        email
+      );
+      console.log("Usuário criado", userCredential.user);
+      dispatch(setUser(userCredential.user));
+    } catch (error) {
+      console.error("Erro ao criar usuário", error);
+      setError(error.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleCreateAccount)} className={styles.createContainer}>
+      <h2 className={styles.createTitle}>Criar conta</h2>
+      <Input
+        {...register('email', {
+          validate: validateEmail
+        })}
+        onBlur={() => trigger('email')}
+        label="Email"
+        error={errors.email?.message}
+      />
+      
+      <Input
+      {...register('password', {
+        validate: validatePassword
+      })}
+        onBlur={() => trigger('password')}
+        label="Senha"
+        error={errors.password?.message}
+      />
+      <Button>Criar conta</Button>
+ 
+    </form>
+  );
+};
+
+export default CreateAccount;
