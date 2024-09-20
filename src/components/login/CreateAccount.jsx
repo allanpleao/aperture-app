8f84c333e65ca2db09549101974db184707aca4d
Allan Andrade <allanpleao@gmail.com>
2024-09-20 14:10:06 -0300

    updated CreateAccount page

--- src/components/login/CreateAccount.jsx
+++ src/components/login/CreateAccount.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from '../form/Input'
 import Button from '../form/Button'
 import styles from './CreateAccount.module.css'
+import { auth } from '../../../firebaseConfig';
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { setUser } from '../../redux/store/slices/authSlice';
+import { useDispatch } from 'react-redux'
+import Error from '../helper/Error'
 
 const CreateAccount = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
 
+  const handleCreateAccount = async(event) => {
+    event.preventDefault();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, password, email);
+      console.log('Usuário criado', userCredential.user);
+      dispatch(setUser(userCredential.user))
+    } catch (error) {
+      console.error('Erro ao criar usuário', error)
+      setError(error.message)
+    }
+  }
+  
   return (
-    <form className={styles.createContainer}>
+    <form onSubmit={handleCreateAccount} className={styles.createContainer}>
       <h2 className={styles.createTitle}>Criar conta</h2>
-      <Input label='Email'/>
-      <Input label='Senha' />
+      <Input value={email} onChange={({ target }) => setEmail(target.value)} label='Email'/>
+      <Input value={password} onChange={({ target }) => setPassword(target.value)} label='Senha' />
       <Input label='Confirmar senha' />
       <Button>Criar conta</Button>
+      {error && <Error error={error} />}
     </form>
   )
 }
