dc3f2640a84809230e14755e1c1f49901e344dd4
Allan Andrade <allanpleao@gmail.com>
2024-10-07 16:41:59 -0300

    updated header to render username and added logout function

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styles from './Header.module.css'
-import logo from '../../assets/logo.png'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./Header.module.css";
+import logo from "../../assets/logo.png";
+import { useSelector } from "react-redux";
+import { signOut } from "firebase/auth";
+import { auth } from "../../../firebaseConfig";
+import { useDispatch } from "react-redux";
+import { clearUser } from "../../redux/store/slices/authSlice";
 
 const Header = () => {
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLogout = async() => {
+    try {
+      await signOut(auth)
+      dispatch(clearUser())
+      navigate('/')
+    } catch (error) {
+      console.error('Erro ai fazer logout:', error)
+    }
+  }
+
   return (
     <header className={styles.header}>
-            <div>
-              <Link to={'/'}><img className={styles.logo} src={logo}/></Link>
-            <Link to={'/'} className={styles.logoText}>Aperture </Link>
-            </div>
-        <nav className={styles.nav}>
-          
-            <Link to={'/login'}>Login</Link>
-            <Link to={'/login'}>About</Link>
-            <Link to={'/login'}>Some text</Link>
-        </nav>
-    </header>
-  )
-}
+      <div>
+        <Link to={"/"}>
+          <img className={styles.logo} src={logo} />
+        </Link>
+        <Link to={"/"} className={styles.logoText}>
+          Aperture{" "}
+        </Link>
+      </div>
+      <nav className={styles.nav}>
+        {user ? (
+          <>
+            <Link onClick={handleLogout}>logout</Link>
+            <Link to={"/user"}>{user.displayName}</Link>
 
-export default Header
+          </>
+        ) : (
+          <Link to={"/login"}>Login</Link>
+        )}
+
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
